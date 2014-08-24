8b217582dedf2c0e11d88ded789b4fd2fa079134
some <some@some-pc>
2014-08-24 22:51:57 +0300

    Added class MyCalc

---
+++ PrivatLabs/src/main/java/com/pb/test/calc/MyCalc.java
@@ -0,0 +1,13 @@
+package com.pb.test.calc;
+
+/**
+ *
+ * @author some
+ */
+public class MyCalc {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
